6043b15d2b4cb20cb0221e4edbc2d8111e70bebe
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-25 19:10:37 -0300

    Fix export app, setting routes and start server

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 class App {
   constructor() {
     this.server = express();
 
     this.middlewares();
+
+    this.routes();
   }
 
   middlewares() {
     this.server.use(express.json());
   }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-export default App = new App();
+module.exports = new App().server;
