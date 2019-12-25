dedc3e56fcd5d4f045bb1b553973257e1fec51d3
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-25 19:26:27 -0300

    Script and setting for debug

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,12 @@ routes.get('/students', (req, res) => {
   return res.json({ msg: 'Hello students' });
 });
 
+routes.post('/students', (req, res) => {
+  return res.json({ msg: 'Hello students post' });
+});
+
+routes.put('/students', (req, res) => {
+  return res.json({ msg: 'Hello students put' });
+});
+
 module.exports = routes;
