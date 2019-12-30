561f2cad5ad1148803d7ef1e2d59f5acc13682c9
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-30 17:39:29 -0300

    Criação RegistrationController e definição rotas

---
+++ src/app/controllers/RegistrationController.js
@@ -0,0 +1,23 @@
+class RegistrationController {
+  async index(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async show(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async delete(req, res) {
+    return res.json({ ok: true });
+  }
+
+  async store(req, res) {
+    return res.json({ ok: true });
+  }
+}
+
+export default new RegistrationController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import StudentController from './app/controllers/StudentController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import GymPlanController from './app/controllers/GymPlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 import authMiddleware from './app/middlewares/auth';
 import idMiddleware from './app/middlewares/CheckId';
 
@@ -35,5 +36,11 @@ routes.get('/gymplans/:planId', GymPlanController.show);
 routes.post('/gymplans', GymPlanController.store);
 routes.delete('/gymplans/:planId', GymPlanController.delete);
 routes.put('/gymplans/:planId', GymPlanController.update);
+
+routes.get('/registrations', RegistrationController.index);
+routes.get('/registrations/:regId', RegistrationController.show);
+routes.post('/registrations', RegistrationController.store);
+routes.delete('/registrations/:regId', RegistrationController.delete);
+routes.put('/registrations/:regId', RegistrationController.update);
 
 export default routes;
