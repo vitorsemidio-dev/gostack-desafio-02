ea1b8b2fee4f28ce21830b26836fabc0e151b945
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-29 21:36:19 -0300

    CRUD GymPlan

--- src/app/controllers/GymPlanController.js
+++ src/app/controllers/GymPlanController.js
@@ -8,22 +8,32 @@ class GymPlanController {
 
   async show(req, res) {
     const { planId } = req.params;
-    return res.json(planId);
+    const plan = await GymPlan.findByPk(planId);
+    if (!plan) {
+      return res.status(404).json({ error: 'Plan does not found' });
+    }
+    return res.json(plan);
   }
 
   async update(req, res) {
     const { planId } = req.params;
-    const { body: plan } = req;
-    return res.json({ planId, plan });
+    const plan = await GymPlan.findByPk(planId);
+    if (!plan) {
+      return res.status(404).json({ error: 'Plan does not found' });
+    }
+
+    const planUpdated = await plan.update(req.body);
+    return res.json(planUpdated);
   }
 
   async delete(req, res) {
-    const { planId } = req.params;
-    return res.json(planId);
+    const { planId: id } = req.params;
+    await GymPlan.destroy({ where: { id } });
+    return res.status(200).json();
   }
 
   async store(req, res) {
-    const { body: plan } = req;
+    const plan = await GymPlan.create(req.body);
     return res.json(plan);
   }
 }
