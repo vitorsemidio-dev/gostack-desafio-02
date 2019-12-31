e8cd0f0b868dd87194e600092f58f1c2c5019b06
Vitor Emidio <vitorsemidio.96@gmail.com>
2019-12-31 12:52:38 -0300

    Validações para atualização de matricula

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { addMonths, isBefore, parseISO } from 'date-fns';
+import { addMonths, isBefore, parseISO, startOfDay } from 'date-fns';
 
 import Registration from '../models/Registration';
 import Student from '../models/Student';
@@ -24,7 +24,9 @@ class RegistrationController {
     const schema = Yup.object().shape({
       student_id: Yup.number(),
       plan_id: Yup.number(),
-      start_date: Yup.date(),
+      start_date: Yup.date().when('plan_id', (plan_id, field) =>
+        !plan_id ? field.required() : field
+      ),
       end_date: Yup.date(),
       price: Yup.number(),
     });
@@ -32,11 +34,19 @@ class RegistrationController {
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
+
+    /**
+     * Check if registration exists
+     */
     const registration = await Registration.findByPk(req.params.regId);
 
     if (!registration) {
       return res.status(404).json({ error: 'Registration does not found' });
     }
+
+    /**
+     * Check if student exists
+     */
     const { student_id } = registration;
 
     const student = await Student.findByPk(student_id);
@@ -45,29 +55,51 @@ class RegistrationController {
       return res.status(404).json({ error: 'Student does not found' });
     }
 
-    const { plan_id } = req.body;
+    /**
+     * Check if registration has already started
+     */
+    const { plan_id, start_date } = req.body;
+    const alreadyStart = isBefore(
+      startOfDay(registration.start_date),
+      new Date()
+    );
 
+    if (plan_id && alreadyStart && !start_date) {
+      return res.status(400).json({
+        error:
+          'New start_date is required to update registration that has already started',
+      });
+    }
+
+    /**
+     * Check if plan exists
+     */
     const plan = await GymPlan.findByPk(plan_id);
 
     if (!plan) {
       return res.status(404).json({ error: 'Plan does not found' });
     }
 
-    const { start_date } = req.body;
-
+    /**
+     * Validation start_date
+     */
     if (start_date && isBefore(parseISO(start_date), new Date())) {
       return res.status(400).json({ error: 'Past dates are not permitted' });
     }
 
+    /**
+     * Save
+     */
     const { price: cost, duration } = plan;
 
     const price = cost * duration;
     const end_date = addMonths(new Date(start_date), duration);
 
     const registro = { plan_id, start_date, end_date, price };
-    const registrationUpdated = await registration.update(registro);
+    // const registrationUpdated = await registration.update(registro);
 
-    return res.json(registrationUpdated);
+    return res.json(registro);
+    // return res.json(registrationUpdated);
   }
 
   async delete(req, res) {
