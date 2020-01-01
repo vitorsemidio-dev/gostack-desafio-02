821f98a178b3d1e07cdc4b14ea745dfb9972882d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-01 14:10:17 -0300

    troca de senha db postgres

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'desafio',
+  password: 'docker',
   database: 'gympoint',
   define: {
     timestamps: true,
