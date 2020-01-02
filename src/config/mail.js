8f10325b8d5a86e5f8b41ddcfc918737390c8d2f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-02 10:50:37 -0300

    Configurações nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd4007d8976df15',
+    pass: '14186824f40174',
+  },
+  default: {
+    from: 'Equipe Gympoint <noreply@gympoint.com.br>',
+  },
+};
