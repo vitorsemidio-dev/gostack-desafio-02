aa4550b954ce4c075e24a451afa8913d4c5365a0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-04 15:21:48 -0300

    Ajuda respondida

--- src/app/controllers/AnswerController.js
+++ src/app/controllers/AnswerController.js
@@ -1,10 +1,22 @@
+import HelpOrder from '../models/HelpOrder';
+
 class AnswerController {
   async index(req, res) {
     return res.json({ answerGet: true });
   }
 
   async store(req, res) {
-    return res.json({ answerPost: true });
+    const { help_id } = req.params;
+    const { answer } = req.body;
+
+    const help = await HelpOrder.findByPk(help_id);
+
+    if (!help) {
+      return res.status(404).json({ error: 'Help does not found' });
+    }
+
+    await help.update({ answer, answer_at: new Date() });
+    return res.json({ help_id, answer, help });
   }
 }
 
