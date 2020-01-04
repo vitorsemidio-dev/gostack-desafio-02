05a410501670cf6d4500aca28c71b2e7bed0988d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-04 14:57:30 -0300

    Fix: created_at

--- src/database/migrations/20200104145154-create-help-orders.js
+++ src/database/migrations/20200104145154-create-help-orders.js
@@ -26,7 +26,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       },
-      create_at: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
