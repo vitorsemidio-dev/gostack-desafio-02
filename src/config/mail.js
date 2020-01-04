0c3b8e78837c1b08032286159a8d57da2e98d44e
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-04 20:05:00 -0300

    Finalização configuração variaveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'd4007d8976df15',
-    pass: '14186824f40174',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gympoint <noreply@gympoint.com.br>',
