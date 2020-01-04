50c923a6a2a93c90bc0f8aeb801c4c77278f5082
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-04 17:33:45 -0300

    Passando dados para template de email

--- src/app/controllers/AnswerController.js
+++ src/app/controllers/AnswerController.js
@@ -1,4 +1,5 @@
 import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
 
 import AnswerMail from '../jobs/AnswerMail';
 import Queue from '../../lib/Queue';
@@ -12,17 +13,28 @@ class AnswerController {
     const { help_id } = req.params;
     const { answer } = req.body;
 
-    const help = await HelpOrder.findByPk(help_id);
+    const help = await HelpOrder.findByPk(help_id, {
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (!help) {
       return res.status(404).json({ error: 'Help does not found' });
     }
 
+    const { question } = help;
+    const { name: student_name, email: student_email } = help.student;
+
     Queue.add(AnswerMail.key, {
-      student_name: 'student_name',
-      student_email: 'student_email',
-      question: 'question',
-      answer: 'answer',
+      student_name,
+      student_email,
+      question,
+      answer,
     });
 
     await help.update({ answer, answer_at: new Date() });
